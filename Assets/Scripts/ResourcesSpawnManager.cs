1979b342836f1582394e00a511ff224f33f59051
Madmozak <razqriz@gmail.com>
2023-08-17 09:59:55 +0200

    prepare scripts gold, iron, manager

---
+++ Assets/Scripts/ResourcesSpawnManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourcesSpawnManager : MonoBehaviour
+{
+    [SerializeField] List<MetalDeposit> ironGoldDepositList = new();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
